1cb79502d9b6a0da927807fce09acb2b747a5d69
William Chong <williamchong007@msn.com>
2018-08-13 18:33:37 +0800

    🚨 Add eslint and fix lint error

--- src/basic.js
+++ src/basic.js
@@ -1,5 +1,4 @@
-module.exports = function({ title, body }) {
-  return `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+module.exports = ({ title, body }) => `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
@@ -204,4 +203,3 @@ table td { border-collapse: collapse; }
 
 </body>
 </html>`;
-}
