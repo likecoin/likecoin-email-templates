f4dbd4f0099ade53cc97ce24debbad97c49ee2a0
Michael Cheung <mc.kingho@gmail.com>
2018-12-27 16:03:22 +0800

    + Add build source

--- dist/index.js
+++ dist/index.js
@@ -2,8 +2,10 @@
 
 var BasicTemplate = require('./basic');
 var PersonalTemplate = require('./personal');
+var MinimalTemplate = require('./minimal');
 
 module.exports = {
   Basic: BasicTemplate,
-  Personal: PersonalTemplate
+  Personal: PersonalTemplate,
+  Minimal: MinimalTemplate
 };
