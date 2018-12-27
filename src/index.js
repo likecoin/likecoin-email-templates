00eb2c4d06e565186feb01a69b60465649a638be
Michael Cheung <mc.kingho@gmail.com>
2018-12-27 14:31:27 +0800

    + Add minimal email template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const BasicTemplate = require('./basic');
 const PersonalTemplate = require('./personal');
+const MinimalTemplate = require('./minimal');
 
 module.exports = {
   Basic: BasicTemplate,
   Personal: PersonalTemplate,
+  Minimal: MinimalTemplate,
 };
