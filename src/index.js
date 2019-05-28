4561c3c849140ae5fb8a8b6b1d1328e8fe1365fe
Michael Cheung <mc.kingho@gmail.com>
2019-05-28 12:42:50 +0800

    ✨ Add babel spread plugin and re-export subpath in main

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const BasicTemplate = require('./basic');
 const PersonalTemplate = require('./personal');
 const MinimalTemplate = require('./minimal');
+const Components = require('./components');
 
 module.exports = {
   Basic: BasicTemplate,
   Personal: PersonalTemplate,
   Minimal: MinimalTemplate,
+  ...Components,
 };
