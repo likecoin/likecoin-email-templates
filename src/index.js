a40163b0458c73b5c2e3c129099d23da078eb361
Michael Cheung <mc.kingho@gmail.com>
2020-03-18 20:12:29 +0800

    ✨ Add minimal template with liker.land logo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const BasicTemplate = require('./basic');
 const PersonalTemplate = require('./personal');
 const MinimalTemplate = require('./minimal');
+const LikerLandMinimalTemplate = require('./liker_land_minimal');
 const Components = require('./components');
 
 module.exports = {
   Basic: BasicTemplate,
   Personal: PersonalTemplate,
   Minimal: MinimalTemplate,
+  LikerLandMinimal: LikerLandMinimalTemplate,
   ...Components,
 };
