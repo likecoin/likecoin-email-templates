0fa5770c94e226561cb8a9bdbcd52ce25eae9f0e
Michael Cheung <mc.kingho@gmail.com>
2020-04-06 14:01:29 +0800

    ✨ Add build dist for header and locale update

--- dist/components/helpCentreHref.js
+++ dist/components/helpCentreHref.js
@@ -5,5 +5,5 @@ module.exports = function () {
       _ref$locale = _ref.locale,
       locale = _ref$locale === undefined ? 'en' : _ref$locale;
 
-  return '\n  <a href="' + (locale === 'zh' ? 'https://docs.like.co/v/zh/' : 'https://docs.like.co/') + '" style="color: #28646E;">\n    ' + (locale === 'zh' ? '幫助中心' : 'help centre') + '\n  </a>\n';
+  return '\n  <a href="' + (locale === 'zh' ? 'https://docs.like.co/v/zh/' : 'https://docs.like.co/') + '" style="color: #28646E;">\n    ' + (locale === 'zh' ? '幫助中心' : 'help center') + '\n  </a>\n';
 };
