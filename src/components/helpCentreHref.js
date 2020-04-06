6c517cecb2120775bd64afb8b7b07739173f0268
Michael Cheung <mc.kingho@gmail.com>
2020-04-06 13:59:48 +0800

    ✏️ Update footer text

--- src/components/helpCentreHref.js
+++ src/components/helpCentreHref.js
@@ -2,6 +2,6 @@ module.exports = ({
   locale = 'en',
 } = {}) => `
   <a href="${locale === 'zh' ? 'https://docs.like.co/v/zh/' : 'https://docs.like.co/'}" style="color: #28646E;">
-    ${locale === 'zh' ? '幫助中心' : 'help centre'}
+    ${locale === 'zh' ? '幫助中心' : 'help center'}
   </a>
 `;
